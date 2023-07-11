d098672bbf9bee79744b75e133fcaca9506a1a23
DESKTOP-C2BUB1S\YES <jihoon708@naver.com>
2023-07-11 22:40:48 +0900

    기본기능 완성

--- app/src/main/java/com/odom/signmaker/MainActivity.kt
+++ app/src/main/java/com/odom/signmaker/MainActivity.kt
@@ -2,19 +2,23 @@ package com.odom.signmaker
 
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.odom.signmaker.Utils.getImageUri
+import com.github.gcacace.signaturepad.views.SignaturePad
 import com.odom.signmaker.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
-    var permission_list = arrayOf<String>(
+    var permission_list = arrayOf(
         android.Manifest.permission.READ_EXTERNAL_STORAGE,
         android.Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
@@ -27,28 +31,53 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(view)
 
+        binding.signaturePad.setOnSignedListener(object : SignaturePad.OnSignedListener {
+            override fun onStartSigning() {
+                binding.btSave.isClickable = false
+            }
+            override fun onSigned() {
+                binding.btSave.isClickable = true
+            }
+
+            override fun onClear() {
+                binding.btSave.isClickable = false
+            }
+        })
+
         binding.btRedraw.setOnClickListener {
             binding.signaturePad.clear()
         }
 
         binding.btSave.setOnClickListener {
 
-            val intent = Intent(Intent.ACTION_SEND)
-
             val signBitmap = binding.signaturePad.signatureBitmap
-            val signBitSvg = binding.signaturePad.signatureSvg
 
             checkPermission()
 
-            val screenshotUri: Uri = Uri.parse(
-                getImageUri(
-                    this, signBitmap
-                ).toString()
-            )
+            val directory = File(Environment.getExternalStorageDirectory(), "SignMaker")
+            directory.mkdirs()
 
-            intent.type = ("image/*")
-            intent.putExtra(Intent.EXTRA_STREAM, screenshotUri)
-            startActivity(Intent.createChooser(intent, "Share image"))
+            val fileName =  System.currentTimeMillis().toString() + ".png"
+            val file = File(directory, fileName)
+
+            try {
+                val outputStream = FileOutputStream(file)
+                signBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                outputStream.flush()
+                outputStream.close()
+
+                // Notify the media scanner about the new image
+                val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+                val contentUri = Uri.fromFile(file)
+                mediaScanIntent.data = contentUri
+                sendBroadcast(mediaScanIntent)
+
+                Toast.makeText(this, R.string.save_success, Toast.LENGTH_SHORT).show()
+            } catch (e: IOException) {
+                e.printStackTrace()
+                Toast.makeText(this, R.string.save_failed, Toast.LENGTH_SHORT).show()
+            }
+
         }
 
     }
@@ -76,7 +105,7 @@ class MainActivity : AppCompatActivity() {
             for (i in grantResults.indices) {
                 //허용됬다면
                 if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "sazz" , Toast.LENGTH_SHORT).show()
+
                 } else {
                     Toast.makeText(applicationContext, "앱 권한 설정하세요", Toast.LENGTH_LONG).show()
                     finish()
